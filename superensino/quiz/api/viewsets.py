92a463b0294b317e4c6a1e04f7d8aa25b73ae2a2
Nonato Sales <nrdesales@hotmail.com>
2021-10-04 11:57:29 -0300

    adicionando mais uma condição

--- superensino/quiz/api/viewsets.py
+++ superensino/quiz/api/viewsets.py
@@ -13,7 +13,7 @@ class QuizViewSet(ModelViewSet):
 
     @action(methods=['post'], detail=True)
     def create_answer(self, request, pk):
-        marks_of_user = MarksOfUser.objects.get(quiz_id=pk)
+        marks_of_user = MarksOfUser.objects.get(quiz_id=pk, user=self.request.user)
         option = Option.objects.get(pk=request.data['id_option'])
         Answer.objects.create(marks_of_user=marks_of_user, option=option)
         return HttpResponse('Resposta Salvas', status.HTTP_200_OK)
